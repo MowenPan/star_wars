4e04517dfdc02731251b863133444381a9e1f089
MowenPan <pan19950817@hotmail.com>
2017-07-26 14:46:02 +0800

    重构check_events()函数,实现上下左右四个方向移动

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,40 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    print(event.key)
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+        # print(event.key)
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+        # print(event.key)
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
+        # print(event.key)
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+        # print(event.key)
+
+
+def check_keyup_events(event, ship):
+    print(event.key)
+    """响应松开按键事件"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+        # print(event.key)
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
+        # print(event.key)
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+        # print(event.key)
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+        # print(event.key)
+
+
 def check_events(ship):
     """响应鼠标和键盘事件"""
     for event in pygame.event.get():
@@ -9,16 +43,10 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
